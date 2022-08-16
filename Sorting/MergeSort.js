04f700769a5f8e7a0e66ab6055718555d55c2cf2
Nandhu <51841792+Narayanan-Nandhu@users.noreply.github.com>
2022-08-16 08:34:48 +0530

    🏀 Improved merge sort

--- Sorting/MergeSort.js
+++ Sorting/MergeSort.js
@@ -1,6 +1,6 @@
 let a = [1,3,7,8], b=[2,4,5,6];
 
-function mergeSort(a,b) {
+function merge(a,b) {
     let combined = [];
     let i=0, j=0;
     while(i<a.length && j<a.length) {
@@ -18,13 +18,34 @@ function mergeSort(a,b) {
         i++;
     }
 
-    while(i<b.length) {
+    while(j<b.length) {
         combined.push(b[j]);
         j++;
     }
     return combined;
 }
 
-let data = mergeSort(a, b);
+// if the two arrays are sorted.
+let data = merge(a, b);
+// console.table(data);
 
-console.table(data);
+
+
+// If the arrays are not sorted. Need to Break it in recursively and then combine it.
+let arr = [3,1,4,2];
+
+function mergeSort(arr) {
+
+    if(arr.length===1) return arr;
+
+    let mid = Math.floor(arr.length/2);
+
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+
+    return merge(mergeSort(left), mergeSort(right));
+
+}
+
+let data2 = mergeSort(arr);
+console.table(data2);
